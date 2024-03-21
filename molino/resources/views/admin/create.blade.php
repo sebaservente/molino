c62f732a92eca69f027d7ccd711929be0d7d7c67
sersebas08 <57241783+sersebas08@users.noreply.github.com>
2024-03-21 20:07:17 -0300

    lo ultimo

--- molino/resources/views/admin/create.blade.php
+++ molino/resources/views/admin/create.blade.php
@@ -9,17 +9,35 @@
 
 @section('admin')
     <h1 class="text-center my-3">Administración Mill</h1>
-    <div>
-        <h2>Crear Producto</h2>
-        <div>
-            <form action="">
-                <div>
-                    <label for="">Titulo</label>
-                    <input type="text">
+    <div class="my-5">
+        <h2 class="text-center">Crear Producto</h2>
+        <div class="div__formCrearProducto ">
+            <form action="" method="post" enctype="multipart/form-data" class="p-3 rounded rounded-lg shadow">
+                <div class="my-2">
+                    <label for="titulo" class="form-label">Titulo</label>
+                    <input type="text" name="titulo" id="titulo" class="form-control">
                 </div>
-                <div>
-                    <label for="">Precio</label>
-                    <input type="text">
+                <div class="my-2">
+                    <label for="precio" class="form-label">Precio</label>
+                    <input type="text" name="precio" id="precio" class="form-control">
+                </div>
+                <div class="my-2">
+                    <label for="descripcion" class="form-label">Descripción</label>
+                    <textarea id="descripcion" name="descripcion" class="form-control"></textarea>
+                </div>
+                <div class="my-2">
+                    <label for="categoria" class="form-label">Categoria</label>
+                    <input type="text" name="categoria" id="categoria" class="form-control">
+                </div>
+
+                <div class="my-2">
+                    <label for="imagen" class="form-label">Imagen</label>
+                    <input type="file" id="imagen" name="imagen" class="form-control">
+                </div>
+
+                <div class="my-2">
+                    <label for="imagen_descripcion" class="form-label">Descripciòn de la imagen</label>
+                    <input type="text" id="imagen_descripcion" name="imagen_descripcion" class="form-control">
                 </div>
                 <div>
                     {{--<button class="btn btn-dark">Ingresar</button>--}}
