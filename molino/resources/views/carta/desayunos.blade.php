b59d7d60138bdd8c89869f5073b6c7305b0191d7
sebastian <85594721+sebaservente@users.noreply.github.com>
2024-03-21 22:48:46 -0300

    css

--- molino/resources/views/carta/desayunos.blade.php
+++ molino/resources/views/carta/desayunos.blade.php
@@ -10,10 +10,10 @@
 @section('main')
     <h1 class="text-center my-3">Mill Desayunos y Meriendas</h1>
 
-    <div class="div_productos">
+    <div class="div__productos">
         @foreach($desayunos as $desayuno)
             <div class="productos my-3 shadow">
-                <div class="img_productos">
+                <div class="img__productos">
                     @if($desayuno->imagen != null && public_path('img/reserva') . '/' . $desayuno->imagen)
                         <picture class="">
                             <source media="(min-width: 751px)" srcset="{{ asset('img/reserva/' . $desayuno->imagen) }}">
@@ -22,16 +22,16 @@
                         </picture>
                     @else
                         <picture class="">
-                            <source media="(min-width: 751px)" srcset="{{ asset('img/molino.png') }}">
-                            <source media="(min-width: 380px)" srcset="{{ asset('img/molino.png') }}">
-                            <img src="{{ asset('img/molino.png') }}" class="w-100" alt="Imagen logo de la marca">
+                            <source media="(min-width: 751px)" srcset="{{ asset('img/cafeCleche.png') }}">
+                            <source media="(min-width: 380px)" srcset="{{ asset('img/cafeCleche.png') }}">
+                            <img src="{{ asset('img/cafeCleche.png') }}" class="w-100" alt="Imagen logo de la marca">
                         </picture>
                     @endif
                 </div>
 
-                <div class="datos_productos">
-                    <p>{{ $desayuno->titulo }}</p>
-                    <p>$ {{ $desayuno->precio }}</p>
+                <div class="datos__productos">
+                    <p class="datos__parrafo">{{ $desayuno->titulo }}</p>
+                    <p class="datos__parrafo">$ <span> {{ $desayuno->precio }}</span></p>
                 </div>
             </div>
 
