c5cf06962fe19f36ded6b6d75a4097cdd1445e55
sersebas08 <57241783+sersebas08@users.noreply.github.com>
2024-03-30 15:03:58 -0300

    dependencias

--- molino/app/Models/Producto.php
+++ molino/app/Models/Producto.php
@@ -5,6 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $producto_id
+ * @property string $titulo
+ * @property int $precio
+ * @property string $descripcion
+ * @property string $categoria
+ * @property string|null $imagen
+ * @property string|null $imagen_descripcion
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereCategoria($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereDescripcion($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereImagen($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereImagenDescripcion($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto wherePrecio($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereProductoId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereTitulo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Producto whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Producto extends Model
 {
     /*use HasFactory;*/
